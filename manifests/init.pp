998e2211180edac57218af49dac63af07168da5b
mh <mh@immerda.ch>
2009-05-17 20:50:06 +0200

    init of module

---
+++ manifests/init.pp
@@ -0,0 +1,16 @@
+#
+# cdb module
+#
+# Copyright 2009, admin(at)immerda.ch
+#
+# This program is free software; you can redistribute 
+# it and/or modify it under the terms of the GNU 
+# General Public License version 3 as published by 
+# the Free Software Foundation.
+#
+
+class cdb {
+  package{'cdb':
+    ensure => installed
+  }
+}
